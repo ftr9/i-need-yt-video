6087856f20c6d1ff510c3d74deb917b1e2f51aa1
ftr9 <ferlandodotel16@gmail.com>
2026-04-09 00:00:07 +0545

    feat: initial commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+## Download YouTube Videos on macOS (Terminal)
+
+This is a simple way to download YouTube videos directly from your terminal on macOS.
+
+Just paste the YouTube video link into the terminal, and it will automatically start downloading and save at desktop
+
+### Why I made this
+
+Normally, I had to type a long command every time, like:
+
+```bash
+yt-dlp -f mp4 -o <video_url>
+```
+
+But honestly, remembering and typing that command again and again is annoying.
+
+So I made this setup to make it easier:
+
+✅ Just paste the URL
+✅ Hit enter
+✅ Video downloads automatically
+
+That’s it. Simple and fast.
---
+++ source/app.tsx
@@ -0,0 +1,152 @@
+import React, {useState} from 'react';
+import {Box, Text, usePaste} from 'ink';
+import childProcess from 'node:child_process';
+import Spinner from 'ink-spinner';
+
+const textDecoder = new TextDecoder();
+
+const LoadingSpinnerWithText = ({children}: {children: string}) => {
+	return (
+		<Text>
+			<Text color={'white'}>
+				<Spinner type="dots" />
+			</Text>{' '}
+			{children}
+		</Text>
+	);
+};
+
+export default function App() {
+	const [result, setResult] = useState('');
+	const [isDownloadingLibrary, setIsDownloadingLibrary] = useState(false);
+	const [isCheckingLibrary, setIsCheckingLibrary] = useState(false);
+	const [isDownloadingVideo, setIsDownloadingVideo] = useState(false);
+	const [downloadingText, setDownloadingText] = useState('');
+	const [error, setError] = useState('');
+
+	const handleLibraryDownload = async () => {
+		return new Promise<boolean>(resolve => {
+			//yt-dlp is not installed at all - try to install it
+			const ytDlpInstallSubProcess = childProcess.spawn('brew', [
+				'install',
+				'yt-dlp',
+			]);
+
+			//installation failed
+			ytDlpInstallSubProcess.on('error', () => resolve(false));
+
+			//success installation
+			ytDlpInstallSubProcess.on('close', _ => {
+				resolve(true);
+			});
+		});
+	};
+
+	const handleVideoDownload = async (downloadUrl: string) => {
+		return new Promise(resolve => {
+			const downloadProcess = childProcess.spawn('yt-dlp', [
+				'-f',
+				'mp4',
+				downloadUrl,
+				'-o',
+				'$HOME/Desktop/%(title)s.%(ext)s',
+			]);
+
+			downloadProcess.stdout.on('data', chunk => {
+				const decodedText = textDecoder.decode(chunk);
+
+				if (decodedText.includes('[download]')) {
+					setDownloadingText(decodedText);
+				}
+			});
+
+			downloadProcess.on('error', () => {
+				resolve(false);
+			});
+
+			downloadProcess.on('close', () => {
+				resolve(true);
+			});
+		});
+	};
+
+	const checkLibaryExistence = async () => {
+		return new Promise<boolean>(resolve => {
+			//check if yt-dlp is installed or not
+			const subProcess = childProcess.spawn('yt-dlp', ['--version']);
+
+			subProcess.stdout.on('data', () => {
+				resolve(true);
+			});
+
+			subProcess.on('error', err => {
+				if (err.message.includes('spawn yt-dlp ENOENT')) {
+					resolve(false);
+				}
+			});
+		});
+	};
+
+	usePaste(async pastedLink => {
+		setIsCheckingLibrary(true);
+		setResult('');
+		setError('');
+		setDownloadingText('');
+
+		const hasLibraryInstalled = await checkLibaryExistence();
+		setIsCheckingLibrary(false);
+
+		if (!hasLibraryInstalled) {
+			setIsDownloadingLibrary(true);
+			const isLibaryDownloaded = await handleLibraryDownload();
+			setIsDownloadingLibrary(false);
+
+			if (!isLibaryDownloaded) {
+				setError('Failed to setup please try again');
+				return;
+			}
+		}
+
+		setIsDownloadingVideo(true);
+		const isVideoDownloaded = await handleVideoDownload(pastedLink);
+		setIsDownloadingVideo(false);
+
+		if (!isVideoDownloaded) {
+			setError('failed to download the video');
+			return;
+		}
+
+		setResult('video download successfully');
+	});
+
+	return (
+		<Box columnGap={2} flexDirection="column">
+			<Text>Download youtube videos</Text>
+			<Text>Paste youtube video link:</Text>
+
+			{isDownloadingVideo && (
+				<LoadingSpinnerWithText>
+					{downloadingText
+						? downloadingText
+						: 'Downloading video please wait ... '}
+				</LoadingSpinnerWithText>
+			)}
+
+			{isDownloadingLibrary && (
+				<LoadingSpinnerWithText>
+					setting up system. don't worry this happens only one time.
+				</LoadingSpinnerWithText>
+			)}
+			{isCheckingLibrary && (
+				<LoadingSpinnerWithText>please wait</LoadingSpinnerWithText>
+			)}
+			{result && (
+				<>
+					<Text color={'green'}>{result} ✅</Text>
+					<Text>paste another url to download again .. ⬇️</Text>
+				</>
+			)}
+			{error && <Text color={'red'}>{error}</Text>}
+		</Box>
+	);
+}
